5ee341d9f945104fd49becab01e204d660462625
shotaroikeda <ikeda.shot@gmail.com>
2016-05-22 09:43:27 -0500

    First commit

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+target 'hackillinois-2017-ios' do
+
+end
+
+target 'hackillinois-2017-iosTests' do
+
+end
+
+target 'hackillinois-2017-iosUITests' do
+
+end
+
---
+++ hackillinois-2017-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hackillinois-2017-ios
+//
+//  Created by Shotaro Ikeda on 5/22/16.
+//  Copyright © 2016 Shotaro Ikeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
