8f1d137d4f72801bd1392081c609ee0a83236428
shotaroikeda <ikeda.shot@gmail.com>
2016-06-21 14:06:47 -0500

    added creating account

---
+++ hackillinois-2017-ios/CreateAccountViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CreateAccountViewController.swift
+//  hackillinois-2017-ios
+//
+//  Created by Shotaro Ikeda on 6/21/16.
+//  Copyright © 2016 Shotaro Ikeda. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
