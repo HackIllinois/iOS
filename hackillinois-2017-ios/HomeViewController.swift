3cdb26fb7777ba448fd11590ac77bed326b7c44d
tommypacker <tommypacker@gmail.com>
2016-12-28 22:34:28 -0800

    Add initial home page

---
+++ hackillinois-2017-ios/HomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HomeViewController.swift
+//  hackillinois-2017-ios
+//
+//  Created by Tommy Yu on 12/28/16.
+//  Copyright © 2016 Shotaro Ikeda. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
