cce39687a5b22b9dac1579b86a75eebfa541ca77
tommypacker <tommypacker@gmail.com>
2016-12-28 23:55:06 -0800

    Added countdown timer to homepage

--- hackillinois-2017-ios/HomeViewController.swift
+++ hackillinois-2017-ios/HomeViewController.swift
@@ -10,10 +10,31 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    var mTimer = Timer()
+    
+    //TODO: Find actual start unix time of event
+    let eventStartUnixTime: Double = 1487937827
+    let currentUnixTime: Double = NSDate().timeIntervalSince1970
+    var timeRemaining: Double = 0.0
+    var secondsLeft: Int = 0
+    var minutesLeft: Int = 0
+    var hoursLeft: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
+        self.timeRemaining = eventStartUnixTime - currentUnixTime
+        secondsLeft = getSeconds(timeInSeconds: timeRemaining)
+        minutesLeft = getMinutes(timeInSeconds: timeRemaining)
+        hoursLeft = getHours(timeInSeconds: timeRemaining)
+        
+        timerLabel.text = getTimeRemainingString(hoursLeft: hoursLeft, minutesLeft: minutesLeft, secondsLeft: secondsLeft)
+        
+        
+        mTimer = Timer.scheduledTimer(timeInterval: 1, target:self, selector: #selector(HomeViewController.updateCounter), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,14 +43,39 @@ class HomeViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func updateCounter() {
+        if(secondsLeft == 0){
+            secondsLeft = 59
+            minutesLeft -= 1
+        }else{
+            secondsLeft -= 1
+        }
+        
+        if(minutesLeft == 0){
+            minutesLeft = 59
+            hoursLeft -= 1
+        }
+        timerLabel.text = getTimeRemainingString(hoursLeft: hoursLeft, minutesLeft: minutesLeft, secondsLeft: secondsLeft)
     }
-    */
+    
+    func getTimeRemainingString(hoursLeft: Int, minutesLeft: Int, secondsLeft: Int) -> String{
+        let secondsString = secondsLeft < 10 ? "0" + String(secondsLeft) : String(secondsLeft)
+        let minutesString = minutesLeft < 10 ? "0" + String(minutesLeft) : String(minutesLeft)
+        return "" + String(hoursLeft) + ":" + minutesString + ":" + secondsString
+    }
+    
+    func getHours(timeInSeconds: Double) -> Int{
+        let hour = (timeInSeconds / Double(3600))
+        return Int(hour)
+    }
 
+    func getMinutes(timeInSeconds: Double) -> Int{
+        let minute = ((timeInSeconds.remainder(dividingBy: 3600.0)) / Double(60))
+        return Int(minute)
+    }
+    
+    func getSeconds(timeInSeconds: Double) -> Int{
+        let second = ((timeInSeconds.remainder(dividingBy: 3600.0)).remainder(dividingBy: 60.0))
+        return Int(second)
+    }
 }
