01bacb07679a6cff1240ac314a014baef540e12f
Minhyuk Park <minpark815@gmail.com>
2017-01-22 12:39:01 +0900

    Add preliminary files for HomeView UI

---
+++ hackillinois-2017-ios/twoLocationsCell.swift
@@ -0,0 +1,12 @@
+//
+//  twoLocationsCell.swift
+//  hackillinois-2017-ios
+//
+//  Created by Minhyuk Park on 1/22/17.
+//  Copyright © 2017 Shotaro Ikeda. All rights reserved.
+//
+
+import UIKit
+
+class twoLocationsCell: UITableViewCell {
+}
