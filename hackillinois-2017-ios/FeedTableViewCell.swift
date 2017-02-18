d04cf989d6c110a7784f71af6beda9dbabda4477
rauhulvarma <rauhul.varma@gmail.com>
2017-02-18 13:33:32 -0600

    abstract duplicate code into LocationButtonContainerTableViewCell

--- hackillinois-2017-ios/FeedTableViewCell.swift
+++ hackillinois-2017-ios/FeedTableViewCell.swift
@@ -8,35 +8,8 @@
 
 import UIKit
 
-class FeedTableViewCell: UITableViewCell, LocationButtonContainer {
+class FeedTableViewCell: LocationButtonContainerTableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var detailLabel: UILabel!
-
-    @IBOutlet weak var verticalStackView: UIStackView?
-    
-    weak var delegate: LocationButtonContainerDelegate?
-    
-    var indexPath: IndexPath?
-    var buttons = [LocationButton]()
-    var locations = [Location]() {
-        didSet {
-            for button in buttons {
-                button.removeFromSuperview()
-            }
-            buttons.removeAll()
-            for (idx, location) in locations.enumerated() {
-                let newButton = UINib(nibName: "LocationButton", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! LocationButton
-                newButton.button.setTitle(location.shortName, for: .normal)
-                newButton.button.tag = idx
-                newButton.button.addTarget(self, action: #selector(locationButtonTapped(button:)), for: .touchUpInside)
-                buttons.append(newButton)
-                verticalStackView?.addArrangedSubview(newButton)
-            }
-        }
-    }
-    
-    func locationButtonTapped(button: UIButton) {
-        delegate?.locationButtonTapped(location: locations[button.tag])
-    }
 }
--- hackillinois-2017-ios/HomeTableViewCells.swift
+++ hackillinois-2017-ios/HomeTableViewCells.swift
@@ -7,33 +7,8 @@
 //
 import UIKit
 
-class HomeTableViewMainCell: UITableViewCell {
+class HomeTableViewMainCell: LocationButtonContainerTableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
-    @IBOutlet weak var verticalStackView: UIStackView!
-    
-    var buttons = [LocationButton]()
-    
-    var locations = [String]() {
-        didSet {
-            for button in buttons {
-                button.removeFromSuperview()
-            }
-            buttons.removeAll()
-            for location in locations {
-                let newButton = UINib(nibName: "LocationButton", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! LocationButton
-                newButton.button.setTitle(location, for: .normal)
-                buttons.append(newButton)
-                verticalStackView.addArrangedSubview(newButton)
-            }
-        }
-    }
-    
-    
-    
-    
-    
-    
-    
 }
 
