17e611cafd796ed6e614c9175802affeb7151b2e
Derek Leung <derek1906@gmail.com>
2017-02-22 01:33:33 -0600

    Hiding tab bar when going to Directions from Schedule

--- hackillinois-2017-ios/ScheduleTableViewController.swift
+++ hackillinois-2017-ios/ScheduleTableViewController.swift
@@ -41,6 +41,7 @@ class ScheduleTableViewController: UITableViewController {
             //navigationController?.navigationBar.backgroundColor = UIColor(red: 28.0/255.0, green: 50.0/255.0, blue: 90.0/255.0, alpha: 1.0)
             vc.isDirectionMode = true
             vc.directionModeTitle = String(location.location_name)
+            vc.hidesBottomBarWhenPushed = true
             navigationController?.pushViewController(vc, animated: true)
         }
     }
