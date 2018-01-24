a8e85a9e076f67cbd48b798c91288ffe746b9a67
Sujay <sujay.patwardhan@gmail.com>
2018-01-23 21:48:16 -0600

    clean up constraints in userDetailView and sort of start tracking service

---
+++ HackIllinois/APIService/HITrackingService.swift
@@ -0,0 +1,37 @@
+//
+//  HITrackingService.swift
+//  HackIllinois
+//
+//  Created by Sujay Patwardhan on 1/23/18.
+//  Copyright © 2018 HackIllinois. All rights reserved.
+//
+
+import Foundation
+import APIManager
+
+class HITrackingService: HIBaseService {
+    
+    override class var baseURL: String {
+        return super.baseURL + "/tracking"
+    }
+    
+    class func login(email: String, password: String) -> APIRequest<HIUserAuth.Contained> {
+        var body = [String: String]()
+        body["email"]    = email
+        body["password"] = password
+        return APIRequest<HIUserAuth.Contained>(service: self, endpoint: "", body: body, method: .POST)
+    }
+    
+    class func githubLoginURL() -> URL {
+        guard let url = URL(string: baseURL + "?isMobile=1") else {
+            fatalError()
+        }
+        return url
+    }
+    
+//    class func found(meta: String, data:) -> APIRequest<HIUserAuth.Contained> {
+//        var body = [String: String]()
+//        body =
+//        return APIRequest<HIUserAuth.Contained>(service: self, endpoint: "", body: body, method: .GET)
+//    }
+}
--- HackIllinois/Controllers/ViewControllers/HIScannerViewController.swift
+++ HackIllinois/Controllers/ViewControllers/HIScannerViewController.swift
@@ -9,6 +9,7 @@
 import Foundation
 import UIKit
 import AVKit
+import APIManager
 
 class HIScannerViewController: HIBaseViewController {
     var captureSession: AVCaptureSession?
