0b3e0f106ea6113098e2705095ba058261a1cf44
rauhul <rauhul.varma@gmail.com>
2018-01-26 14:45:20 -0600

    fix naming

--- HackIllinois/Controllers/FlowControllers/HIMenuController.swift
+++ HackIllinois/Controllers/FlowControllers/HIMenuController.swift
@@ -126,10 +126,9 @@ extension HIMenuController {
         menuItems.trailingAnchor.constraint(equalTo: menu.safeAreaLayoutGuide.trailingAnchor, constant: -59).isActive = true
         menuItemsHeight = menuItems.heightAnchor.constraint(equalToConstant: 0)
         menuItemsHeight.isActive = true
-        
-        let tapToClose = UITapGestureRecognizer(target: self, action: #selector(HIMenuController.close(_:)))
-        overlayView.addGestureRecognizer(tapToClose)
-        
+
+        let overlayViewTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(HIMenuController.close(_:)))
+        overlayView.addGestureRecognizer(overlayViewTapGestureRecognizer)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
