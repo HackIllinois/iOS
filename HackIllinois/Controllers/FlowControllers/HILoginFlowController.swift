1b5ebf2503037ace39579ba23eb3430d2358f039
Sujay Patwardhan <sujay.patwardhan@gmail.com>
2018-10-14 22:58:45 -0700

    bring dev up to date with master (#105)

--- HackIllinois/Controllers/FlowControllers/HILoginFlowController.swift
+++ HackIllinois/Controllers/FlowControllers/HILoginFlowController.swift
@@ -4,6 +4,10 @@
 //
 //  Created by Rauhul Varma on 12/2/17.
 //  Copyright © 2017 HackIllinois. All rights reserved.
+//  This file is part of the Hackillinois iOS App.
+//  The Hackillinois iOS App is open source software, released under the University of
+//  Illinois/NCSA Open Source License. You should have received a copy of
+//  this license in a file with the distribution.
 //
 
 import UIKit
