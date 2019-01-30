50126ba4f3c9438e144473d74fc37a863310a543
Arnav Sankaran <arnavsankaran@gmail.com>
2019-01-29 22:58:58 -0600

    animation updates

--- HackIllinois/ViewControllers/HIBaseViewController.swift
+++ HackIllinois/ViewControllers/HIBaseViewController.swift
@@ -102,44 +102,42 @@ extension HIBaseViewController: UITableViewDataSource {
 
 // MARK: - NSFetchedResultsControllerDelegate
 extension HIBaseViewController: NSFetchedResultsControllerDelegate {
-//    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-//        tableView?.beginUpdates()
-//    }
-//
-//    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-//        switch type {
-//        case .insert:
-//            guard let insertIndexPath = newIndexPath else { return }
-//            tableView?.insertRows(at: [insertIndexPath], with: .fade)
-//        case .delete:
-//            guard let deleteIndexPath = indexPath else { return }
-//            tableView?.deleteRows(at: [deleteIndexPath], with: .fade)
-//        case .update:
-//            guard let updateIndexPath = indexPath else { return }
-//            tableView?.reloadRows(at: [path], with: .fade)
-//        case .move:
-//            guard let fromIndexPath = indexPath, let toIndexPath = newIndexPath else { return }
-//            tableView?.insertRows(at: [toIndexPath], with: .fade)
-//            tableView?.deleteRows(at: [fromIndexPath], with: .fade)
-//        }
-//    }
-//
-//    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
-//        switch type {
-//        case .insert:
-//            tableView?.insertSections([sectionIndex], with: .fade)
-//        case .delete:
-//            tableView?.deleteSections([sectionIndex], with: .fade)
-//        case .update:
-//            tableView?.reloadSections([sectionIndex], with: .fade)
-//        case .move:
-//            break
-//        }
-//    }
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView?.beginUpdates()
+    }
+
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            guard let insertIndexPath = newIndexPath else { return }
+            tableView?.insertRows(at: [insertIndexPath], with: .fade)
+        case .delete:
+            guard let deleteIndexPath = indexPath else { return }
+            tableView?.deleteRows(at: [deleteIndexPath], with: .fade)
+        case .update:
+            guard let updateIndexPath = indexPath else { return }
+            tableView?.reloadRows(at: [updateIndexPath], with: .fade)
+        case .move:
+            guard let fromIndexPath = indexPath, let toIndexPath = newIndexPath else { return }
+            tableView?.moveRow(at: fromIndexPath, to: toIndexPath)
+        }
+    }
+
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert:
+            tableView?.insertSections([sectionIndex], with: .fade)
+        case .delete:
+            tableView?.deleteSections([sectionIndex], with: .fade)
+        case .update:
+            tableView?.reloadSections([sectionIndex], with: .fade)
+        case .move:
+            break
+        }
+    }
 
     func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        guard let tableView = tableView else { return }
-        tableView.reloadData()
+        tableView?.endUpdates()
     }
 
     func animateTableViewReload() {
