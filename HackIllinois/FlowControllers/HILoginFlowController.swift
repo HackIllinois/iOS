9398f3bbf48b4b556cd5e690f53505ac4e679fdd
PatrickKan <patrickkan24@gmail.com>
2019-02-16 02:35:09 -0600

    Added back requestAuth change

--- HackIllinois/FlowControllers/HILoginFlowController.swift
+++ HackIllinois/FlowControllers/HILoginFlowController.swift
@@ -16,6 +16,7 @@ import Lottie
 import SafariServices
 import SwiftKeychainAccess
 import HIAPI
+import UserNotifications
 
 class HILoginFlowController: UIViewController {
     // MARK: - Properties
@@ -181,6 +182,9 @@ private extension HILoginFlowController {
                 } else {
                     DispatchQueue.main.async {
                         NotificationCenter.default.post(name: .loginUser, object: nil, userInfo: ["user": user])
+
+                        //Requests for notification authorization upon user login
+                        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (_, _) in }
                     }
                 }
             } catch {
