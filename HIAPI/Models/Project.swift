19b9e04373a1c085ffe2255290962561d1e35532
Patrick Kan <patrickkan24@gmail.com>
2019-12-20 23:53:32 -0600

    Set up services and coredata for projects

---
+++ HIAPI/Models/Project.swift
@@ -0,0 +1,36 @@
+//
+//  Project.swift
+//  HackIllinois
+//
+//  Created by HackIllinois Team on 12/20/19.
+//  Copyright © 2019 HackIllinois. All rights reserved.
+//  This file is part of the Hackillinois iOS App.
+//  The Hackillinois iOS App is open source software, released under the University of
+//  Illinois/NCSA Open Source License. You should have received a copy of
+//  this license in a file with the distribution.
+//
+
+import Foundation
+import APIManager
+
+public struct ProjectContainer: Decodable, APIReturnable {
+    public let projects: [Project]
+}
+
+public struct Project: Codable {
+    internal enum CodingKeys: String, CodingKey {
+        case id
+        case projectName = "name"
+        case mentors
+        case location
+        case tags
+        case code
+    }
+
+    public let id: String
+    public let projectName: String
+    public let mentors: [String]
+    public let location: Location
+    public let tags: [String]
+    public let code: String
+}
