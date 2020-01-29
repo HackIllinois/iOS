55d40ada2c6d711bc8434703d5bac458398b9a7f
Patrick Kan <patrickkan24@gmail.com>
2020-01-28 23:31:24 -0600

    made check in an event to be scanned

--- HackIllinois/ViewControllers/HIEventDetailViewController.swift
+++ HackIllinois/ViewControllers/HIEventDetailViewController.swift
@@ -19,6 +19,7 @@ import APIManager
 class HIEventDetailViewController: HIBaseViewController {
     // MARK: - Properties
     private static let scannerViewController = HIEventScannerViewController()
+    private static let checkInScannerViewController = HICheckInScannerViewController()
     var event: Event?
 
     // MARK: Views
@@ -86,6 +87,13 @@ extension HIEventDetailViewController {
 
     @objc func didSelectScanButton(_ sender: UIBarButtonItem) {
         guard let event = event else { return }
+        
+        // Check in is an event, identified by name
+        if event.name.caseInsensitiveCompare("Check In") == .orderedSame {
+            navigationController?.pushViewController(HIEventDetailViewController.checkInScannerViewController, animated: true)
+            return
+        }
+        
         let now = Date()
         if event.startTime.addingTimeInterval(-15*60) > now {
             let message = "The scanning period for this event has not begun; scanning begins 15 minutes before the event."
