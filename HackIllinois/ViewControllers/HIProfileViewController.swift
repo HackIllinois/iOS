f79e33f173dbbd31e936192a33897eacb71d4455
hsahn0323 <hyosang2@illinois.edu>
2021-02-01 23:03:26 -0600

    Added background and edit button for profile page TODO: 	Add attributes to label fonts, images, etc. 		Implement other components

--- HackIllinois/ViewControllers/HIProfileViewController.swift
+++ HackIllinois/ViewControllers/HIProfileViewController.swift
@@ -12,10 +12,26 @@ import CoreData
 
 class HIProfileViewController: HIBaseViewController {
     // MARK: - Properties
+    private let editButton = HIButton {
+        $0.tintHIColor = \.baseText
+        $0.backgroundHIColor = \.clear
+        $0.baseImage = #imageLiteral(resourceName: "Pencil")
+    }
+    private let profilePictureView = UIImageView()
+    private let userNameLabel = HILabel {
+        $0.textHIColor = \.baseText
+        
+    }
 }
 
+// MARK: - UIViewController
 extension HIProfileViewController {
     override func loadView() {
+        super.loadView()
+        self.navigationItem.rightBarButtonItem = editButton.toBarButtonItem()
+        // To add action
+        editButton.constrain(width: 22, height: 22)
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
