62d7514641ccbc4c3149c5e2d5e549f505544227
hsahn0323 <hyosang2@illinois.edu>
2021-02-24 13:13:38 -0600

    Fixed point and time view. Applied scroll view over description contents.

--- HackIllinois/ViewControllers/HIProfileViewController.swift
+++ HackIllinois/ViewControllers/HIProfileViewController.swift
@@ -46,9 +46,9 @@ class HIProfileViewController: HIBaseViewController {
     private let profileTimeSubtitleView = HILabel(style: .profileDescription) {
         $0.text = "time zone"
     }
-    private let profileTimeStackView = UIStackView()
-    private let profilePointsStackView = UIStackView()
-    private let profileStatStackView = UIStackView()
+//    private let profileTimeStackView = UIStackView()
+//    private let profilePointsStackView = UIStackView()
+//    private let profileStatStackView = UIStackView()
     private let profileStatView = HIView() // Will be initialized in ViewController extension (.axis = .horizontal)
     // Good resource for UIStackView: https://stackoverflow.com/questions/43904070/programmatically-adding-views-to-a-uistackview
     private let profileDescriptionView = HILabel(style: .profileDescription) {
@@ -64,7 +64,6 @@ class HIProfileViewController: HIBaseViewController {
     }
     private let profileInterestsView = HILabel() // Will be replaced by UICollectionView
 
-
     private let userNameLabel = HILabel {
         $0.textHIColor = \.baseText
     }
@@ -79,63 +78,81 @@ extension HIProfileViewController {
         editButton.constrain(width: 22, height: 22)
         // Setup contentView (Display summary)
         view.addSubview(contentView)
-        contentView.constrain(to: view.safeAreaLayoutGuide, topInset: 12, trailingInset: -12, leadingInset: 12)
+        contentView.constrain(to: view.safeAreaLayoutGuide, topInset: 12, trailingInset: -12, bottomInset: -12, leadingInset: 12)
 
         contentView.addSubview(profilePictureView)
         profilePictureView.constrain(to: contentView, topInset: 0)
         profilePictureView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-
         profilePictureView.constrain(width: 100, height: 100)
-        
         contentView.addSubview(profileNameView)
         profileNameView.topAnchor.constraint(equalTo: profilePictureView.bottomAnchor, constant: 12).isActive = true
         profileNameView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-        
         contentView.addSubview(profileSubtitleView)
         profileSubtitleView.topAnchor.constraint(equalTo: profileNameView.bottomAnchor).isActive = true
-//        profileSubtitleView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
         profileSubtitleView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-        // Below code makes every content except image disappear
-//        contentView.addSubview(profileStatView)
-//        profileStatView.topAnchor.constraint(equalTo: profileSubtitleView.bottomAnchor, constant: 20).isActive = true
-//        profileStatView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-//        profileStatView.addSubview(profileStatStackView)
-//        profileStatStackView.constrain(to: profileStatView)
-//        profileStatStackView.axis = .horizontal
-//        profileStatStackView.distribution = .fillEqually
-//        profileStatStackView.topAnchor.constraint(equalTo: profileSubtitleView.bottomAnchor, constant: 19).isActive = true
-//        profileStatStackView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-        
-//        profileStatStackView.addArrangedSubview(profileTimeStackView)
-//        profileTimeStackView.addArrangedSubview(profileTimeView)
-//        profileTimeStackView.addArrangedSubview(profileTimeSubtitleView)
-//        profileStatStackView.addArrangedSubview(profilePointsStackView)
-//        profilePointsStackView.addArrangedSubview(profilePointsView)
-//        profilePointsStackView.addArrangedSubview(profilePointsSubtitleView)
-        
-        contentView.addSubview(profileDescriptionView)
+        let profileStatStackView = UIStackView()
+        contentView.addSubview(profileStatStackView)
+        loadStatView(profileStatStackView: profileStatStackView)
+        // Reference: https://medium.com/swift-productions/create-a-uiscrollview-programmatically-xcode-12-swift-5-3-f799b8280e30
+        let descriptionScrollView = UIScrollView()
+        contentView.addSubview(descriptionScrollView)
+        descriptionScrollView.translatesAutoresizingMaskIntoConstraints = false
+        descriptionScrollView.topAnchor.constraint(equalTo: profileStatStackView.bottomAnchor, constant: 20).isActive = true
+        descriptionScrollView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        descriptionScrollView.widthAnchor.constraint(equalTo: contentView.widthAnchor).isActive = true
+        descriptionScrollView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        let descriptionContentView = UIView()
+        descriptionScrollView.addSubview(descriptionContentView)
+        descriptionContentView.translatesAutoresizingMaskIntoConstraints = false
+        descriptionContentView.topAnchor.constraint(equalTo: descriptionScrollView.topAnchor).isActive = true
+        descriptionContentView.bottomAnchor.constraint(equalTo: descriptionScrollView.bottomAnchor).isActive = true
+        descriptionContentView.widthAnchor.constraint(equalTo: descriptionScrollView.widthAnchor).isActive = true
+        descriptionContentView.centerXAnchor.constraint(equalTo: descriptionScrollView.centerXAnchor).isActive = true
+        descriptionContentView.addSubview(profileDescriptionView)
+        loadDescriptionView(descriptionContentView: descriptionContentView)
+    }
+    
+    func loadStatView(profileStatStackView: UIStackView) {
+        profileStatStackView.axis = .horizontal
+        profileStatStackView.distribution = .fillEqually
+        profileStatStackView.topAnchor.constraint(equalTo: profileSubtitleView.bottomAnchor, constant: 19).isActive = true
+        profileStatStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        profileStatStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        profileStatStackView.translatesAutoresizingMaskIntoConstraints = false
+        let profilePointsStackView = UIStackView()
+        profilePointsStackView.axis = .vertical
+        profilePointsStackView.distribution = .fillEqually
+        profilePointsStackView.alignment = .center
+        profileStatStackView.addArrangedSubview(profilePointsStackView)
+        profilePointsStackView.addArrangedSubview(profilePointsView)
+        profilePointsStackView.addArrangedSubview(profilePointsSubtitleView)
+        let profileTimeStackView = UIStackView()
+        profileTimeStackView.axis = .vertical
+        profileTimeStackView.distribution = .fillEqually
+        profileTimeStackView.alignment = .center
+        profileStatStackView.addArrangedSubview(profileTimeStackView)
+        profileTimeStackView.addArrangedSubview(profileTimeView)
+        profileTimeStackView.addArrangedSubview(profileTimeSubtitleView)
+    }
+    func loadDescriptionView(descriptionContentView: UIView) {
         profileDescriptionView.lineBreakMode = .byWordWrapping
         profileDescriptionView.numberOfLines = 0
-        profileDescriptionView.topAnchor.constraint(equalTo: profileSubtitleView.bottomAnchor, constant: 100).isActive = true
-        profileDescriptionView.constrain(to: contentView, trailingInset: 0, leadingInset: 0)
-//        profileDescriptionView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
-        
-        contentView.addSubview(profileDiscordImageView)
+        profileDescriptionView.constrain(to: descriptionContentView, topInset: 0, trailingInset: 0, leadingInset: 0)
+        descriptionContentView.addSubview(profileDiscordImageView)
         profileDiscordImageView.constrain(width: 24, height: 17)
         profileDiscordImageView.topAnchor.constraint(equalTo: profileDescriptionView.bottomAnchor, constant: 29).isActive = true
-        profileDiscordImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
-        contentView.addSubview(profileDiscordUsernameView)
+        profileDiscordImageView.leadingAnchor.constraint(equalTo: descriptionContentView.leadingAnchor).isActive = true
+        descriptionContentView.addSubview(profileDiscordUsernameView)
         profileDiscordUsernameView.lineBreakMode = .byWordWrapping
         profileDiscordUsernameView.numberOfLines = 0
         profileDiscordUsernameView.topAnchor.constraint(equalTo: profileDiscordImageView.topAnchor).isActive = true
-        profileDiscordUsernameView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        profileDiscordUsernameView.trailingAnchor.constraint(equalTo: descriptionContentView.trailingAnchor).isActive = true
         profileDiscordImageView.trailingAnchor.constraint(equalTo: profileDiscordUsernameView.leadingAnchor, constant: -9).isActive = true
-        contentView.addSubview(profileInterestsLabelView)
-//        profileDiscordImageView.bottomAnchor.constraint(equalTo: profileInterestsLabelView.topAnchor).isActive = true
+        descriptionContentView.addSubview(profileInterestsLabelView)
         profileDiscordUsernameView.bottomAnchor.constraint(equalTo: profileInterestsLabelView.topAnchor, constant: -10).isActive = true
-        contentView.addSubview(profileInterestsView)
+        descriptionContentView.addSubview(profileInterestsView)
         profileInterestsView.topAnchor.constraint(equalTo: profileInterestsLabelView.bottomAnchor).isActive = true
-        profileInterestsView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        profileInterestsView.bottomAnchor.constraint(equalTo: descriptionContentView.bottomAnchor).isActive = true
     }
     override func viewDidLoad() {
         super.viewDidLoad()
