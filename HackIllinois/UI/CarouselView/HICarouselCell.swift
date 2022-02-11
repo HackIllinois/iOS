1a7aa50ae6398ef4057e675111f616f149fc60ec
Ashay Parikh <ashayparikh22@gmail.com>
2022-02-11 00:38:37 -0600

    updated screenshots

--- HackIllinois/UI/CarouselView/HICarouselCell.swift
+++ HackIllinois/UI/CarouselView/HICarouselCell.swift
@@ -43,7 +43,7 @@ private extension HICarouselCell {
     func setUpImage() {
         addSubview(imageView)
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.bottomAnchor.constraint(equalTo: titleLabel.topAnchor, constant: -20).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: titleLabel.topAnchor, constant: 0).isActive = true
         imageView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
         imageView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
         imageView.heightAnchor.constraint(equalToConstant: frame.height * 0.75).isActive = true
@@ -68,7 +68,7 @@ private extension HICarouselCell {
         descriptionLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 0).isActive = true
         descriptionLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: 0).isActive = true
         if UIDevice.current.userInterfaceIdiom == .pad {
-            descriptionLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50).isActive = true
+            descriptionLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -100).isActive = true
         } else {
             descriptionLabel.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
         }
