af956dd7ea6287de66669c13b70addce8ea37a83
Louis Qian <yuq4@illinois.edu>
2022-12-22 12:20:17 -0600

    fix:Opening Brace Spacing Violation

--- HackIllinois/SwiftUI/UI/HICarouselSwiftUIView.swift
+++ HackIllinois/SwiftUI/UI/HICarouselSwiftUIView.swift
@@ -9,7 +9,7 @@
 import Foundation
 import SwiftUI
 
-struct CarouselData: Hashable{
+struct CarouselData: Hashable {
     let image: UIImage?
     let titleText: String
     let descriptionText: String
