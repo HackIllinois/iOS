79b40c93ff724eadef5e2bb67385b6646627da6d
Louis Qian <yuq4@illinois.edu>
2023-02-03 15:26:21 -0600

    feat(carousel): added custom paging

--- HackIllinois/SwiftUI/UI/HICarouselSwiftUIView.swift
+++ HackIllinois/SwiftUI/UI/HICarouselSwiftUIView.swift
@@ -21,28 +21,60 @@ struct CarouselData: Hashable {
 
 struct HICarouselSwiftUIView: View {
     var carouselData: [CarouselData]
+    @State private var currentIndex = 0
     var body: some View {
-        TabView {
-            ForEach(carouselData, id: \.self) { carousel in
-                VStack {
-                    Image(uiImage: carousel.image!)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: UIDevice.current.userInterfaceIdiom == .pad ? 600: 360)
-                        .padding()
-                    HILableSUI(text: carousel.titleText, style: .onboardingTitle)
-                        .frame(width: 20, height: 20)
-                    Text(carousel.descriptionText)
-                        .bold()
-                        .foregroundColor(.white)
-                        .multilineTextAlignment(.center)
-                        .padding(.top, 20)
+        VStack {
+            TabView(selection: $currentIndex) {
+                ForEach(0..<carouselData.count, id: \.self) { index in
+                    VStack {
+                        Image(uiImage: carouselData[index].image!)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: UIDevice.current.userInterfaceIdiom == .pad ? 600: 360)
+                            .padding()
+                        HILableSUI(text: carouselData[index].titleText, style: .onboardingTitle)
+                            .frame(width: 20, height: 20)
+                        Text(carouselData[index].descriptionText)
+                            .bold()
+                            .foregroundColor(.white)
+                            .multilineTextAlignment(.center)
+                            .padding(.top, 20)
+                    }
+                    .tag(index)
                 }
             }
-        }.tabViewStyle(.page)
-            .frame(width: UIDevice.current.userInterfaceIdiom == .pad ? 500: 300)
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+            HITabIndicator(count: carouselData.count, current: $currentIndex)
+                .offset(y: -50)
+        }
+        .frame(width: UIDevice.current.userInterfaceIdiom == .pad ? 800: 300)
     }
 }
+
+struct HITabIndicator: View {
+    var count: Int
+    @Binding var current: Int
+    var body: some View {
+        HStack {
+            ForEach(0 ..< count, id: \.self) {index in
+                ZStack {
+                    if current == index {
+                        Circle()
+                            .fill(Color.white)
+                    } else {
+                        Circle()
+                            .fill(Color.clear)
+                            .overlay(
+                                Circle()
+                                    .stroke(Color.white, lineWidth: 3)
+                            )
+                    }
+                }.frame(width: 10, height: 10)
+            }
+        }
+    }
+}
+
 // MARK: - SwiftUI => HILabel
 struct HILableSUI: UIViewRepresentable {
     var text: String
@@ -57,8 +89,8 @@ struct HILableSUI: UIViewRepresentable {
     }
 }
 
-//struct CarouselSwiftUIView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        HICarouselSwiftUIView()
-//    }
-//}
+struct CarouselSwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        HIOnboardingView()
+    }
+}
