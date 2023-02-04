f51645569cc3b82fb252fe2fe0b8df617696a72f
Louis Qian <yuq4@illinois.edu>
2023-02-03 23:06:33 -0600

    fix: color duplicates

--- HackIllinois/UI/HIAppearance.swift
+++ HackIllinois/UI/HIAppearance.swift
@@ -102,12 +102,7 @@ struct HIAppearance: Equatable {
     private static let white = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
     private static let darkBlue = #colorLiteral(red: 0.05098039216, green: 0.2196078431, blue: 0.4862745098, alpha: 1)
     private static let transparent = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0)
-    
-    // Hack 2023 color palette
-    private static let white = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
     private static let darknavy = #colorLiteral(red: 0.1137254902, green: 0.1490196078, blue: 0.2666666667, alpha: 1)
-
-    // Hack 2023 color palette
     private static let vegetarian = #colorLiteral(red: 0.2156862745, green: 0.8705882353, blue: 0.8039215686, alpha: 1)
     private static let vegan = #colorLiteral(red: 0.9882352941, green: 0.862745098, blue: 0.5607843137, alpha: 1)
     private static let glutenfree = #colorLiteral(red: 0.9764705882, green: 0.5843137255, blue: 0.3411764706, alpha: 1)
