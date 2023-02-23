70952d96e23c04c0751ab99b6387a33a3daf8562
Lasya <44485522+lasyaneti@users.noreply.github.com>
2023-02-22 22:12:05 -0600

    merging Vincent/fix alert (#523)

--- HackIllinois/FlowControllers/HIApplicationStateController.swift
+++ HackIllinois/FlowControllers/HIApplicationStateController.swift
@@ -39,6 +39,7 @@ class HIApplicationStateController {
     func initalize() {
         window.makeKeyAndVisible()
         resetPersistentDataIfNeeded()
+        reset2023IfNeeded()
         recoverUserIfPossible()
         recoverProfileIfPossible()
         onboardingViewController.shouldDisplayAnimationOnNextAppearance = user == nil
@@ -50,6 +51,15 @@ class HIApplicationStateController {
 
 // MARK: - API
 extension HIApplicationStateController {
+    func reset2023IfNeeded() {
+        let didReset = UserDefaults.standard.object(forKey: "didReset2023") as? Bool ?? false
+        if !didReset {
+            _ = Keychain.default.purge()
+            HICoreDataController.shared.purge()
+            UserDefaults.standard.set(true, forKey: "didReset2023")
+            logoutUser()
+        }
+    }
     func resetPersistentDataIfNeeded() {
         guard !UserDefaults.standard.bool(forKey: HIConstants.APPLICATION_INSTALLED_KEY) else { return }
         _ = Keychain.default.purge()
