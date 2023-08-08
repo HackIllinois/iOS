e0c117648168e2513f6e7c9cbe6f27e1794efbf5
Lasya <44485522+lasyaneti@users.noreply.github.com>
2023-08-08 12:18:56 -0700

    Empty commit to trigger build/lint workflow

--- HackIllinois/SwiftUI/UI/HICarouselSwiftUIView.swift
+++ HackIllinois/SwiftUI/UI/HICarouselSwiftUIView.swift
@@ -7,7 +7,7 @@
 //  This file is part of the Hackillinois iOS App.
 //  The Hackillinois iOS App is open source software, released under the University of
 //  Illinois/NCSA Open Source License. You should have received a copy of
-//  this license in a file with the distribution.
+//  this license in a file with the distribution. 
 //
 
 import Foundation
