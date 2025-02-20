784dec95545f7711b3a88debac7be9af8b4ec7e5
anushkasankaran <anushka.sankaran@gmail.com>
2025-02-19 21:16:02 -0600

    Update call body

--- HIAPI/Services/StaffService.swift
+++ HIAPI/Services/StaffService.swift
@@ -32,12 +32,13 @@ public final class StaffService: BaseService {
         return APIRequest<StaffAttendanceContainer>(service: self, endpoint: "attendance/", body: body, headers: headers, method: .POST)
     }
     
-    public static func recordUserAttendance(userToken: String, staffToken: String, eventId: String) -> APIRequest<UserAttendanceContainer> {
+    public static func recordUserAttendance(userQR: String, staffToken: String, eventId: String) -> APIRequest<UserAttendanceContainer> {
         var body = HTTPBody()
-        body["attendeeJWT"] = userToken
         body["eventId"] = eventId
+        body["attendeeQRCode"] = userQR
+        
         var headers = HTTPHeaders()
-        headers["Authorization"] = userToken
+        headers["Authorization"] = staffToken
         return APIRequest<UserAttendanceContainer>(service: self, endpoint: "scan-attendee/", body: body, headers: headers, method: .PUT)
     }
 }
